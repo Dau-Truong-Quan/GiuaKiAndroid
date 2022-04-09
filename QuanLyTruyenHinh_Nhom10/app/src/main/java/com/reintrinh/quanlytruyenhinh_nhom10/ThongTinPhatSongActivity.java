093d44049fc34c8700350b19bec61d414580c13f
trinhnhi1306 <yayoi1306@gmail.com>
2022-04-09 12:32:41 +0700

    Add back home button

--- QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/ThongTinPhatSongActivity.java
+++ QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/ThongTinPhatSongActivity.java
@@ -1,8 +1,10 @@
 package com.reintrinh.quanlytruyenhinh_nhom10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ThongTinPhatSongActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class ThongTinPhatSongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_tin_phat_song);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
