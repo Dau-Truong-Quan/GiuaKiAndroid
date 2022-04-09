9daf065ed9bb9459a8437915f88d27d5ac3804d0
phucpvz <senaarata12345@gmail.com>
2022-04-09 22:58:25 +0700

    item_thong_tin_phat_song

--- QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/adapter/ThongTinPhatSongAdapter.java
+++ QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/adapter/ThongTinPhatSongAdapter.java
@@ -1,4 +1,63 @@
 package com.reintrinh.quanlytruyenhinh_nhom10.adapter;
 
-public class ThongTinPhatSongAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.reintrinh.quanlytruyenhinh_nhom10.R;
+import com.reintrinh.quanlytruyenhinh_nhom10.model.ThongTinPhatSong;
+
+import java.util.List;
+
+public class ThongTinPhatSongAdapter extends RecyclerView.Adapter<ThongTinPhatSongAdapter.ThongTinPhatSongViewHolder> {
+
+    private List<ThongTinPhatSong> thongTinPhatSongList;
+
+    @NonNull
+    @Override
+    public ThongTinPhatSongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_thong_tin_phat_song, parent, false);
+        return new ThongTinPhatSongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ThongTinPhatSongViewHolder holder, int position) {
+        ThongTinPhatSong thongTinPhatSong = thongTinPhatSongList.get(position);
+        if (thongTinPhatSong == null) {
+            return;
+        }
+        holder.tvNgayPS.setText(thongTinPhatSong.getNgayPhatSong().toString());
+//        holder.tvTenBTV.setText(thongTinPhatSong.get);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (thongTinPhatSongList != null) {
+            return thongTinPhatSongList.size();
+        }
+        return 0;
+    }
+
+    public class ThongTinPhatSongViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgThongTinPS;
+        private TextView tvNgayPS, tvTenBTV, tvThoiLuong;
+        private ImageView imgSua, imgXoa;
+
+        public ThongTinPhatSongViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgThongTinPS = itemView.findViewById(R.id.img_thong_tin_phat_song);
+            tvNgayPS = itemView.findViewById(R.id.tv_ngay_phat_song);
+            tvTenBTV = itemView.findViewById(R.id.tv_bien_tap_vien);
+            tvThoiLuong = itemView.findViewById(R.id.tv_thoi_luong);
+            imgSua = itemView.findViewById(R.id.img_sua);
+            imgXoa = itemView.findViewById(R.id.img_xoa);
+        }
+    }
 }
--- QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/model/ThongTinPhatSong.java
+++ QuanLyTruyenHinh_Nhom10/app/src/main/java/com/reintrinh/quanlytruyenhinh_nhom10/model/ThongTinPhatSong.java
@@ -1,4 +1,63 @@
 package com.reintrinh.quanlytruyenhinh_nhom10.model;
 
+import java.util.Date;
+
 public class ThongTinPhatSong {
+
+    private String maPhatSong;
+    private ChuongTrinh chuongTrinh;
+    private BienTapVien bienTapVien;
+    private Date ngayPhatSong;
+    private int thoiLuong;
+
+    public ThongTinPhatSong() {
+    }
+
+    public ThongTinPhatSong(String maPhatSong, ChuongTrinh chuongTrinh, BienTapVien bienTapVien, Date ngayPhatSong, int thoiLuong) {
+        this.maPhatSong = maPhatSong;
+        this.chuongTrinh = chuongTrinh;
+        this.bienTapVien = bienTapVien;
+        this.ngayPhatSong = ngayPhatSong;
+        this.thoiLuong = thoiLuong;
+    }
+
+    public String getMaPhatSong() {
+        return maPhatSong;
+    }
+
+    public void setMaPhatSong(String maPhatSong) {
+        this.maPhatSong = maPhatSong;
+    }
+
+    public ChuongTrinh getChuongTrinh() {
+        return chuongTrinh;
+    }
+
+    public void setChuongTrinh(ChuongTrinh chuongTrinh) {
+        this.chuongTrinh = chuongTrinh;
+    }
+
+    public BienTapVien getBienTapVien() {
+        return bienTapVien;
+    }
+
+    public void setBienTapVien(BienTapVien bienTapVien) {
+        this.bienTapVien = bienTapVien;
+    }
+
+    public Date getNgayPhatSong() {
+        return ngayPhatSong;
+    }
+
+    public void setNgayPhatSong(Date ngayPhatSong) {
+        this.ngayPhatSong = ngayPhatSong;
+    }
+
+    public int getThoiLuong() {
+        return thoiLuong;
+    }
+
+    public void setThoiLuong(int thoiLuong) {
+        this.thoiLuong = thoiLuong;
+    }
 }
